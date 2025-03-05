17e8de038ca997b32a57a720cb4abcfc138a3df6
anaualima <alissandralima@gmail.com>
2025-03-05 10:11:33 -0300

    aply if/else

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -2,5 +2,18 @@ package candidatura;
 public class ProcessoSeletivo {
     public static void main(String[] args) throws Exception {
         System.out.println("Processo Seletivo");
+        analisarCandidato(1900.0);
+        analisarCandidato(2200.0);
+        analisarCandidato(2000.0);
+    }
+    static void analisarCandidato(double salarioPretendido) {
+        double salarioBase = 2000.0;
+        if(salarioBase > salarioPretendido){
+            System.out.println("LIGAR PARA O CANDIDATO");
+        }else if(salarioBase==salarioPretendido) {
+            System.out.println("LIGAR COM CONTRAPROPOSTA");
+        }else {
+            System.out.println("AGUARDANDO RESULTADO DOS DEMAIS CANDIDATOS");
+        }
     }
 }
