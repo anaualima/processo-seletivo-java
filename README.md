793ddc085ddec6575846c314e595a8bc50a56b82
anaualima <alissandralima@gmail.com>
2025-03-06 17:26:43 -0300

    apply while

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ## Processo Seletivo
 
-Projeto realizado durante o curso da plataforma DIO. O desafio consiste em criar um processo seletivo, aplicando técnicas de POO, uso de condicionais e demais conhecimentos básicos da programação. 
+Projeto realizado durante o curso da plataforma DIO. O desafio consiste em criar um processo seletivo, aplicando técnicas de POO, uso de condicionais e demais conhecimentos básicos da programação.
--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -1,11 +1,43 @@
 package candidatura;
+
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
     public static void main(String[] args) throws Exception {
-        System.out.println("Processo Seletivo");
-        analisarCandidato(1900.0);
-        analisarCandidato(2200.0);
-        analisarCandidato(2000.0);
+        // System.out.println("Processo Seletivo");
+        // analisarCandidato(1900.0);
+        // analisarCandidato(2200.0);
+        // analisarCandidato(2000.0);
+        selecaoCandidatos();
     }
+
+    static void selecaoCandidatos() {
+        String [] candidatos = {
+            "ALICE", "BRUNO", "CARLA", "DANIEL", "EDUARDA", 
+            "FELIPE", "GABRIELA", "HENRIQUE", "ISABELA", "JOÃO"
+        };
+
+        int candidatosSelecionados = 0;
+        int candidatoAtual = 0;
+        double salarioBase = 2000.0;
+        while(candidatosSelecionados < 5 && candidatoAtual < candidatos.length){
+            String candidato = candidatos[candidatoAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O candidato "+ candidato + " solicitou o salároio " + salarioPretendido+".");
+            if(salarioBase >= salarioPretendido){
+                System.out.println("O candidato " + candidato + " foi selecionado para a vaga.");
+                candidatosSelecionados++;
+            }
+            candidatoAtual++;
+            
+        }   
+    }
+
+    static double valorPretendido() {
+        return ThreadLocalRandom.current().nextDouble(1800.0, 2200.0);
+    }
+
     static void analisarCandidato(double salarioPretendido) {
         double salarioBase = 2000.0;
         if(salarioBase > salarioPretendido){
